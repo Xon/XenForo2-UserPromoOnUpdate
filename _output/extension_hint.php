fcbd1d0e84a65a5b5bd9e6cedb263cfd458b8c24
Liam W <l.s.w5559@gmail.com>
2017-06-24 00:18:52 +0100

    Missing files

---
+++ _output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\UserPromoOnUpdate\XF\Repository
+{
+	class XFCP_UserGroupPromotion extends \XF\Repository\UserGroupPromotion {}
+}
